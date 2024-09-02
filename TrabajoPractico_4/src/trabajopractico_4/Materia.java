dbbeb05ed78055bd94bf6bdd7166869ef237a557
Hector Santiago Subidia <santiagosubidia@gmail.com>
2024-09-02 15:50:05 -0300

    Trabajo Practico Nro 4
    trabajo practico nro4 completo, queda revisarlo

---
+++ TrabajoPractico_4/src/trabajopractico_4/Materia.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabajopractico_4;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author santi
+ */
+public class Materia {
+    int idMateria;
+    String nombre;
+    int anio;
+
+    public Materia(int idMateria, String nombre, int anio) {
+        this.idMateria = idMateria;
+        this.nombre = nombre;
+        this.anio = anio;
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + this.idMateria;
+        hash = 97 * hash + Objects.hashCode(this.nombre);
+        hash = 97 * hash + this.anio;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Materia other = (Materia) obj;
+        if (this.idMateria != other.idMateria) {
+            return false;
+        }
+        if (this.anio != other.anio) {
+            return false;
+        }
+        return Objects.equals(this.nombre, other.nombre);
+    }
+    
+    
+}
