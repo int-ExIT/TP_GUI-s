cd37d1358267a508d154fad99cb01010c152ed2f
saiimoon <saiescu845@gmail.com>
2024-09-02 17:19:24 -0300

    inicio trabajo

---
+++ TrabajoPractico_5lab/src/main/java/com/mycompany/trabajopractico_5lab/Directorio.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.trabajopractico_5lab;
+
+import java.util.TreeMap;
+
+/**
+ *
+ * @author IK
+ */
+public class Directorio {
+    TreeMap<Long, Contacto> ListaCont;
+
+    public Directorio() { ListaCont = new TreeMap< Long, Contacto>();
+    }
+
+ void agregarContacto(){
+     
+ }   
+ void buscarContacto(){
+ 
+ }
+ void bucarTelefono(){
+     
+ }
+ void buscarContactos(){
+     
+ }
+ void borrarContacto(){
+     
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+}
---
+++ TrabajoPractico_5lab/src/main/java/com/mycompany/trabajopractico_5lab/TrabajoPractico_5lab.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabajopractico_5lab;
+
+/**
+ *
+ * @author IK
+ */
+public class TrabajoPractico_5lab {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
