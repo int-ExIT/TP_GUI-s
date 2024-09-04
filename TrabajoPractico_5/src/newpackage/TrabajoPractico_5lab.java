9abfca169c882fa01de1de1646f9e1e873715d31
MartinGonL <martingontero@hotmail.com>
2024-09-04 17:47:26 -0300

    cambio de maven a clasico
    cargue todos los datos ya tipeados

---
+++ TrabajoPractico_5/src/newpackage/TrabajoPractico_5lab.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package newpackage;
+
+/**
+ *
+ * @author IK
+ */
+public class TrabajoPractico_5lab {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
