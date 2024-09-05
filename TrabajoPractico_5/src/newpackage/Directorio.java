895276b3ebc37bedbbc6c31c384e17d610888fd4
Hector Santiago Subidia <santiagosubidia@gmail.com>
2024-09-04 21:02:01 -0300

    Clase 04/09 (Trabajo Practico 5)
    Trabajamos sobre el Trabajo Practico 5

--- TrabajoPractico_5/src/newpackage/Directorio.java
+++ TrabajoPractico_5/src/newpackage/Directorio.java
@@ -4,41 +4,71 @@
  */
 package newpackage;
 
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 /**
  *
  * @author IK
  */
 public class Directorio {
-    TreeMap<Long, Contacto> ListaCont;
+
+    private TreeMap<Long, Contacto> ListaCont;
 //constructor
-    public Directorio() { ListaCont = new TreeMap< Long, Contacto>();
+
+    public Directorio() {
+        ListaCont = new TreeMap< Long, Contacto>();
     }
 
- void agregarContacto(Long tel, Contacto cont){
-     ListaCont.put(tel, cont);
- }   
- Contacto buscarContacto(Long tel){
- for(Long telef: ListaCont.keySet()){
-     if (telef == tel){
-         return ListaCont.get(tel);}
- } 
- return null;
- }
+    public void agregarContacto(Long tel, Contacto cont) {
+        ListaCont.put(tel, cont);
+    }
 
- 
- void bucarTelefono(){
-     
- }
- void buscarContactos(){
-     
- }
- void borrarContacto(){
-     
- }
- 
- 
+    public Contacto buscarContacto(Long tel) {
+        
+        for (Long telef : ListaCont.keySet()) {
+            if (telef == tel) {
+                return ListaCont.get(tel);
+            }
+        }
+        return null;
+    }
+
+    public Set bucarTelefono(String ape) {
+        Set<Long> listNumeros = new TreeSet<Long>();
+        
+        for (Map.Entry<Long,Contacto> entry: ListaCont.entrySet()) {
+            if (entry.getValue().getApellido().equals(ape)) {
+                listNumeros.add(entry.getKey());
+            }
+        }
+        
+        
+        return listNumeros;
+    }
+
+    public ArrayList buscarContactos(String ciu) {
+        ArrayList<Contacto> listContacto = new ArrayList();
+        
+        for (Contacto c : ListaCont.values()) {
+            if (c.getCiudad().equals(ciu)) {
+                listContacto.add(c);
+            }
+        }
+        
+        return listContacto;
+    }
+
+    //Quedamos para debatir
+    public void borrarContacto(Long tel) {
+        ListaCont.remove(tel);
+    }
+
+
  
  
  
