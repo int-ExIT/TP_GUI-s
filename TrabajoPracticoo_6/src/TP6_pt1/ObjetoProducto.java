8fca8c2bcd54234fee96f0e0bd25c3ad2cd18c58
saiimoon <saiescu845@gmail.com>
2024-09-11 18:41:23 -0300

    Parte1Terminado

---
+++ TrabajoPracticoo_6/src/TP6_pt1/ObjetoProducto.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TP6_pt1;
+
+/**
+ *
+ * @author IK
+ */
+public class ObjetoProducto {
+
+    private String nombre;
+    private String categoria;
+    private double precio;
+
+    public ObjetoProducto() {
+    }
+
+    public ObjetoProducto(String nombre, String categoria, double precio) {
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" + "nombre=" + nombre + ", categoria=" + categoria + ", precio=" + precio + '}';
+    }
+
+}
